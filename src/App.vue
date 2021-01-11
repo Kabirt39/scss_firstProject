7dde71c1b1d590d2d1e1e252b375ac5ac4f48bfd
Kabirt39 <mdshahjahan692@gmail.com>
2021-01-11 15:26:00 +0600

    scss first project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<div id="app font_family">
+			<div class="header-area">
+				<img src="../src/assets/header.jpg" alt="img">
+				<h1>Le Catering</h1>
+			</div>
+			<div class="about-catering similar-width">
+				<div class="about-catering-left box">
+					<img src="../src/assets/about_left.jpg" alt="about-img">
+				</div>
+				<div class="about-catering-right box">
+					<h1>About Catering</h1>
+					<h5>Tradition since 1889</h5>
+					<p>The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing
+						elit consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+						nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+						pariatur.We only use <span>seasonal</span> ingredients.
+					</p>
+					<h6>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+						est laborum consectetur adipiscing elit,
+						sed do eiusmod temporincididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.
+					</h6>
+				</div>
+			</div>
+			
+			<div class="similar-width add_margin-bottom">
+				<div class="container_border"></div>
+			</div>
+			
+			<div class="our_menu similar-width">
+				<hr>
+				<div class="our_menu-left box2">
+					<h1>Our Menu</h1>
+					<h3>Bread Basket</h3>
+					<p>Assortment of fresh baked fruit breads and muffins 5.50</p>
+					<h3>Honey Almond Granola with Fruits</h3>
+					<p>Natural cereal of honey toasted oats, raisins, almonds and dates 7.00</p>
+					<h3>Belgian Waffle</h3>
+					<p>Vanilla flavored batter with malted flour 7.50</p>
+					<h3>Scrambled eggs</h3>
+					<p>Scrambled eggs, roasted red pepper and garlic, with green onions 7.50</p>
+					<h3>Blueberry Pancakes</h3>
+					<p>With syrup, butter and lots of berries 8.50</p>
+				</div>
+				<div class="about-catering-left box2">
+					<img src="../src/assets/header2.jpg" alt="about-img">
+				</div>
+			</div>
+			
+			<div class="similar-width add_margin-bottom">
+				<div class="container_border"></div>
+			</div>
+			
+			<div class="contact-area">
+				<hr>
+				<h1>Contact</h1>
+				<p>We offer full-service catering for any event, large or small. We understand your needs and we will cater the food to satisfy the biggerst criteria of them all, both look and taste. Do not hesitate to contact us.</p>
+				<h3>Catering Service, 42nd Living St, 43043 New York, NY</h3>
+				<p>You can also contact us by phone 00553123-2323 or email catering@catering.com, or you can send us a message here:</p>
+				<form action="#">
+					<input class="w3-input w3-padding-16" type="text" placeholder="Name" required="" name="Name">
+					<input class="w3-input w3-padding-16" type="number" placeholder="How many people" required="" name="People">
+					<input class="w3-input w3-padding-16" type="datetime-local" placeholder="Date and time" required="" name="date" value="2020-11-16T20:00">
+					<input class="w3-input w3-padding-16" type="text" placeholder="Message \ Special requirements" required="" name="Message">
+					<button class="w3-button w3-light-grey w3-section" type="submit">SEND MESSAGE</button>
+				</form>
+			</div>
+			<div class="mixin-area">
+				<p>Catering Service, 42nd Living St, 43043 New York, NY</p>
+				<div class="mixin1">
+					<p>Catering Service, 42nd Living St, 43043 New York, NY</p>
+					<div class="mixin2">
+						<p>Catering Service, 42nd Living St, 43043 New York, NY</p>
+						<div class="mixin3">
+							<p>Catering Service, 42nd Living St, 43043 New York, NY</p>
+							<div class="mixin4">
+								<p>Catering Service, 42nd Living St, 43043 New York, NY</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<footer class="footer-area">
+				<p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+			</footer>
+		
+		</div>
+	</div>
+	
+	
+	
+	
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import project from '../src/components/project_one';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components:{
+	
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/project_one.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "project_one.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
